3291c4c48ae8012937f2f15a2f4b14823a77789a
namantiwari07 <namantiwari2015@gmail.com>
2018-01-18 00:45:22 -0500

    animations applied

--- js/main.js
+++ js/main.js
@@ -58,3 +58,78 @@ $(document).ready(function () {
         $('html,body').scrollTo(this.hash, this.hash);
     });
 });
+
+
+// Animation effect
+
+//effect for the front pages
+$(document).ready(function () {
+
+    var slideInLeft = 'animated slideInLeft';
+    var slideInRight = 'animated slideInRight';
+
+    $('.sub-main').addClass(slideInLeft);
+    $('.sub-date').addClass(slideInRight); 
+
+});
+        
+// Animation for the weeding details section
+
+$(window).scroll(function () {
+
+    var storyPos = $('.aboutText').offset().top;
+    // top offset of the Babout text class i.e. the paragraphs
+    var venuePos = $('.weddingVenue').offset().top;
+    // top offset for the venue section
+    var detailsPos = $('.invitationMsg').offset().top;
+    // top offset for the wedding details section
+    // var msgPos = $('.contact').offset().top;
+    // top offset for the invitation section
+    var detailsHeight = $(this).height();
+    var topOfWindow = $(window).scrollTop();
+
+
+    // for the our story section
+    $('.aboutImage').each(function () { // it will apply the effect on the venue class
+        if (storyPos < topOfWindow + detailsHeight && storyPos + detailsHeight > topOfWindow) {
+            $(this).addClass("animated fadeInUp");
+        } else {
+            $(this).removeClass("animated fadeInUp");
+        }
+    });
+
+
+    // for the venue section
+    $('.venue').each(function () { // it will apply the effect on the venue class
+        if (venuePos < topOfWindow + detailsHeight && venuePos + detailsHeight > topOfWindow) {
+            $(this).addClass("animated rotateIn");
+        } else {
+            $(this).removeClass("animated rotateIn");
+        }
+    });
+
+    // for the weeding details section
+    $('.newevent').each(function () { // it will apply the effect on the newevent class
+        if (detailsPos < topOfWindow + detailsHeight && detailsPos + detailsHeight > topOfWindow) {
+            $(this).addClass("animated slideInLeft");
+        } else {
+            $(this).removeClass("animated slideInLeft");
+        }
+    });
+    $('.place').each(function () { // it will apply the effect on the place class
+        if (detailsPos < topOfWindow + detailsHeight && detailsPos + detailsHeight > topOfWindow) {
+            $(this).addClass("animated slideInRight");
+        } else {
+            $(this).removeClass("animated slideInRight");
+        }
+    });
+
+    // // for the invitation message section
+    // $('.msg').each(function () {
+    //     if (msgPos < topOfWindow + detailsHeight && msgPos + detailsHeight > topOfWindow) {
+    //         $(this).addClass("animated fadeIn");
+    //     } else {
+    //         $(this).removeClass("animated fadeIn");
+    //     }
+    // });
+});
